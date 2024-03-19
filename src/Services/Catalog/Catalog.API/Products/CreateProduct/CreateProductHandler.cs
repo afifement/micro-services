784215ba928c52c99db5eab79c8135cd23e41c23
Afif LAMLOUMI <afif.lamloumi@trendit.fr>
2024-03-19 15:04:43 +0100

    carter,mapster,minimal api,mediatr

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace Catalog.API.Products.CreateProduct;
+public record CreateProductCommand(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+              : ICommand<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        //Create Product entity from command object 
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+        //save database
+
+        //return result 
+        return new CreateProductResult(Guid.NewGuid());
+    }
+}

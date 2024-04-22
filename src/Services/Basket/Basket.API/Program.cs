b1901e320598c592959bed88acebdd75fef880b4
Afif LAMLOUMI <afif.lamloumi@trendit.fr>
2024-04-22 22:58:00 +0200

    add endpoint

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,9 +1,41 @@
-var builder = WebApplication.CreateBuilder(args);
 
+
+var builder = WebApplication.CreateBuilder(args);
+var assembly = typeof(Program).Assembly;
 // Add services to the container
 
-var app = builder.Build();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 
-// Configure the HTTP request pipeline
+});
+builder.Services.AddValidatorsFromAssembly(assembly);
+builder.Services.AddCarter();
+builder.Services
+    .AddMarten(opts =>
+    {
+        opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    })
+    .UseLightweightSessions();
+if (builder.Environment.IsDevelopment())
+{
+    //builder.Services.InitializeMartenWith<CatalogInitialData>();
+}
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder.Services.AddHealthChecks()
+                .AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
+var app = builder.Build();
+ 
+//configure the Http request pipeline
+app.MapCarter();
+app.UseExceptionHandler(options => { });
+app.UseHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
 
 app.Run();

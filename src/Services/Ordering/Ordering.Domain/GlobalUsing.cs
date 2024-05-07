9010fe2753db640fb833b7194088737cbdf98fc0
LAMLOUMI AFIF <afifl@ext.Essilor.com>
2024-05-07 17:07:05 +0200

    add domain layer

--- src/Services/Ordering/Ordering.Domain/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Domain/GlobalUsing.cs
@@ -1,1 +1,5 @@
 ﻿global using MediatR;
+global using Ordering.Domain.Abstractions;
+global using Ordering.Domain.Models; 
+global using Ordering.Domain.Enums; 
+global using Ordering.Domain.Models.ValueObjects;
